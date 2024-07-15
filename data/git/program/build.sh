e124120526ce1fcc499fc5b90309c00a6f10774b
J. Ryan Stinnett <jryans@gmail.com>
2024-07-15 16:10:54 +0100

    Use same steps for all Clang levels in Git program build script

--- data/git/program/build.sh
+++ data/git/program/build.sh
@@ -16,51 +16,10 @@ export LLVM_COMPILER_PATH="$(llvm release-clang-lldb-13.0.0)/bin"
 TARGET_NAME="git"
 TARGET_PATH="${TARGET_NAME}"
 
-# Clang O0
+# Clang
 
-level="O0"
-version="13"
-echo "## Building \`${TARGET_NAME}\` (Clang ${version}, ${level})"
-
-make clean
-git clean -f
-
-## Build for O0
-make \
-  CC=wllvm \
-  CFLAGS="${CC_COMMON_OPTS} ${CC_CLANG_OPTS} ${CC_O0_OPTS}"
-
-## Extract bitcode for O0
-extract-bc ${TARGET_PATH}
-mkdir -p "${SCRIPT_DIR}/clang/${version}/${level}"
-cp \
-  ${TARGET_PATH}.bc \
-  "${SCRIPT_DIR}/clang/${version}/${level}/${TARGET_NAME}.bc"
-
-## Disassemble O0 bitcode for debugging
-$(llvm release-clang-lldb-${version}.0.0 llvm-dis) \
-  "${SCRIPT_DIR}/clang/${version}/${level}/${TARGET_NAME}.bc"
-
-## Apply mem2reg only
-mkdir -p "${SCRIPT_DIR}/clang/${version}/${level}-mem2reg"
-$(llvm release-clang-lldb-${version}.0.0 opt) \
-  -o "${SCRIPT_DIR}/clang/${version}/${level}-mem2reg/${TARGET_NAME}.bc" \
-  --mem2reg \
-  "${SCRIPT_DIR}/clang/${version}/${level}/${TARGET_NAME}.bc"
-
-## Disassemble O0 plus mem2reg bitcode for debugging
-$(llvm release-clang-lldb-${version}.0.0 llvm-dis) \
-  "${SCRIPT_DIR}/clang/${version}/${level}-mem2reg/${TARGET_NAME}.bc"
-
-# Store program binary
-cp \
-  ${TARGET_PATH} \
-  "${SCRIPT_DIR}/clang/${version}/${level}/${TARGET_NAME}"
-
-# Clang O1+
-
-  levels=(O1 O2)
-versions=(13 13)
+  levels=(O0 O1 O2)
+versions=(13 13 13)
 
 for i in ${!levels[*]}; do
   level=${levels[$i]}
