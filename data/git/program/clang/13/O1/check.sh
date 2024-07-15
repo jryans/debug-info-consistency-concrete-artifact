e43f6adcb2c089dd398144fbca4b1eaf22b64a44
J. Ryan Stinnett <jryans@gmail.com>
2024-07-15 12:29:37 +0100

    Separate Git program check scripts for each version and level

---
+++ data/git/program/clang/13/O1/check.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+set -ux
+
+SCRIPT_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" &> /dev/null && pwd)
+
+# Expects to run from this script's directory
+if [ "${PWD}" != "${SCRIPT_DIR}" ]; then
+  echo "Does not appear to be the expected directory, abort!"
+  exit
+fi
+
+TARGET_NAME="git"
+source "${SCRIPT_DIR}/../../../../../vars.sh"
+
+# Clang O1 checked against O0
+level="O1"
+version="18"
+echo "## Checking debug consistency of \`${TARGET_NAME}\` (Clang ${version}, ${level})"
+${CHECK} \
+  ../O0/${TARGET_NAME}.bc \
+  ${TARGET_NAME}.bc \
+  ${CHECK_OPTS} \
+  --max-functions=10 \
+  "$@"
--- data/git/program/reproduce.sh
+++
@@ -1,3 +0,0 @@
-# TODO: Restructure this to capture a separate log for each version and level
-script -q check.md ./check.sh
-dos2unix -q -f check.md
