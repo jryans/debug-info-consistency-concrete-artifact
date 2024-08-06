cbae8412c9e4c237037f9f7c5dad22ef48010875
J. Ryan Stinnett <jryans@gmail.com>
2024-08-06 16:01:04 +0100

    Record SPEC CPU 2017 prep steps

--- data/spec-cpu-2017/505.mcf_r/prepare.sh
+++ data/spec-cpu-2017/505.mcf_r/prepare.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
 set -eux
 
-if [ "${PWD##*/}" != "build_base_mytest.0000" ]; then
-  echo "Does not appear to be the expected directory, abort!"
-  exit
-fi
+# TODO: Arrange a real script for this
 
-# Nothing to do, `make` is all you need
+# 1. Extract SPEC CPU 2017 distribution media
+# 2. Review quick start docs
+#    (https://www.spec.org/cpu2017/Docs/quick-start.html)
+# 3. Build the `505.mcf_r` benchmark:
+#    runcpu --config=clang-macos.cfg --action=build 505.mcf_r
+# 4. Run the `build.sh` here in the generated `build_base_mytest.0000` directory
