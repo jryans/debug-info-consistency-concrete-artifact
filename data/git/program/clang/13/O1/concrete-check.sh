61f3257e5d148b4db12b482a35cab18e930ca14f
J. Ryan Stinnett <jryans@gmail.com>
2024-09-03 18:54:23 +0100

    Collect diff stats as initial concrete check

--- data/git/program/clang/13/O1/concrete-check.sh
+++ data/git/program/clang/13/O1/concrete-check.sh
@@ -16,3 +16,9 @@ source "${SCRIPT_DIR}/../../../../../vars.sh"
 level="O1"
 version="13"
 echo "## Checking concrete debug consistency of \`${TARGET_NAME}\` (Clang ${version}, ${level})"
+
+mkdir -p diff
+git diff --no-index --diff-algorithm=myers --numstat -- ../O0/concrete-trace concrete-trace > diff/myers
+git diff --no-index --diff-algorithm=minimal --numstat -- ../O0/concrete-trace concrete-trace > diff/minimal
+git diff --no-index --diff-algorithm=patience --numstat -- ../O0/concrete-trace concrete-trace > diff/patience
+git diff --no-index --diff-algorithm=histogram --numstat -- ../O0/concrete-trace concrete-trace > diff/histogram
