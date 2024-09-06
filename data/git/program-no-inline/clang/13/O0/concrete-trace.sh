9e61d15e999425b86e6433a8ba0073b0b000437a
J. Ryan Stinnett <jryans@gmail.com>
2024-09-06 16:43:53 +0100

    Update concrete trace scripts without inlining

--- data/git/program-no-inline/clang/13/O0/concrete-trace.sh
+++ data/git/program-no-inline/clang/13/O0/concrete-trace.sh
@@ -12,29 +12,42 @@ fi
 TARGET_NAME="git"
 source "${SCRIPT_DIR}/../../../../../vars.sh"
 
+# Analyse `git` in the context of separate repo to reduce trace noise
+REPO_PATH="${HOME}/Projects/ripgrep"
+
 # Collect trace for Clang O0
 level="O0"
 version="13"
 echo "## Collecting concrete trace of \`${TARGET_NAME}\` (Clang ${version}, ${level})"
 
-mkdir -p concrete-trace/log
-(
-  cd concrete-trace/log;
-  env \
-    ${CON_COLLECT_INSTRUMENTATION} \
-    "$@" \
-    ../../${TARGET_NAME} \
-    log -n 10 \
-    > stdout
-)
+# Each execution of the target to analyse
+executions=(log show)
+log_COMMAND="log -n 10"
+show_COMMAND="show -p"
 
-mkdir -p concrete-trace/show
-(
-  cd concrete-trace/show;
-  env \
-    ${CON_COLLECT_INSTRUMENTATION} \
-    "$@" \
-    ../../${TARGET_NAME} \
-    show -p \
-    > stdout
-)
+# Different trace variants to collect
+# These map to different trace options in `vars.sh`
+trace_variants=(default rfld ld)
+
+for execution in ${executions[*]}; do
+  execution_command="${execution}_COMMAND"
+  echo "### Analysing execution of \`${TARGET_NAME} ${!execution_command}\`"
+
+  for trace_variant in ${trace_variants[*]}; do
+    trace_variant_opts="CON_TRACE_${trace_variant}_OPTS"
+    echo "#### Collecting trace variant \`${trace_variant}\`: ${!trace_variant_opts}"
+
+    mkdir -p concrete-trace/${execution}/${trace_variant}
+    (
+      cd concrete-trace/${execution}/${trace_variant};
+      env \
+        ${!trace_variant_opts} \
+        ${CON_COLLECT_INSTRUMENTATION} \
+        "$@" \
+        ../../../${TARGET_NAME} \
+        -C ${REPO_PATH} \
+        ${!execution_command} \
+        > stdout
+    )
+  done
+done
--- data/git/program-no-inline/clang/13/O1/concrete-check.sh
+++ data/git/program-no-inline/clang/13/O1/concrete-check.sh
@@ -16,3 +16,21 @@ source "${SCRIPT_DIR}/../../../../../vars.sh"
 level="O1"
 version="13"
 echo "## Checking concrete debug consistency of \`${TARGET_NAME}\` (Clang ${version}, ${level})"
+
+mkdir -p diff
+git diff --no-index --diff-algorithm=myers -- \
+  ../O0/concrete-trace concrete-trace \
+  | diffstat -t -S ../O0/concrete-trace \
+  > diff/myers
+git diff --no-index --diff-algorithm=minimal -- \
+  ../O0/concrete-trace concrete-trace \
+  | diffstat -t -S ../O0/concrete-trace \
+  > diff/minimal
+git diff --no-index --diff-algorithm=patience -- \
+  ../O0/concrete-trace concrete-trace \
+  | diffstat -t -S ../O0/concrete-trace \
+  > diff/patience
+git diff --no-index --diff-algorithm=histogram -- \
+  ../O0/concrete-trace concrete-trace \
+  | diffstat -t -S ../O0/concrete-trace \
+  > diff/histogram
