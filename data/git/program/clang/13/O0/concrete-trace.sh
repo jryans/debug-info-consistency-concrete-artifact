202597b61a37868a94d67d91a98850193fd588a3
J. Ryan Stinnett <jryans@gmail.com>
2024-09-06 13:28:29 +0100

    Add all locations disabled trace variant

--- data/git/program/clang/13/O0/concrete-trace.sh
+++ data/git/program/clang/13/O0/concrete-trace.sh
@@ -27,7 +27,7 @@ show_COMMAND="show -p"
 
 # Different trace variants to collect
 # These map to different trace options in `vars.sh`
-trace_variants=(default rfld)
+trace_variants=(default rfld ld)
 
 for execution in ${executions[*]}; do
   execution_command="${execution}_COMMAND"
