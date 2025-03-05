7aaf3064ffe921e18b1371d45716ef7d2b686f8f
J. Ryan Stinnett <jryans@gmail.com>
2025-03-05 13:13:44 +0000

    Tweak test dependency cleanup

--- data/git/program-no-inline/clang/13/O1/concrete-trace-tests.sh
+++ data/git/program-no-inline/clang/13/O1/concrete-trace-tests.sh
@@ -26,8 +26,8 @@ trace_variants=(default)
 # Remove dependencies from past runs
 rm -f "../${TARGET_NAME}"
 rm -f "../${TARGET_NAME}.dwarf"
-rm -f "../bin-wrappers"
-rm -f "../templates/blt"
+rm -rf "../bin-wrappers"
+rm -rf "../templates/blt"
 rm -f "../GIT-BUILD-OPTIONS"
 rm -f "../t/helper/test-tool"
 
