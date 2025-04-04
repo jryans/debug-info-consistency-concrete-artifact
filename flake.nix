16d4d2aff2ecb0659235a700b4a5bc08894c6c4a
J. Ryan Stinnett <jryans@gmail.com>
2025-04-04 12:00:13 +0100

    Add basic Nix flake

---
+++ flake.nix
@@ -0,0 +1,20 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
+  };
+
+  outputs = { self, nixpkgs, ... }: let
+    system = "x86_64-linux";
+  in {
+    devShells."${system}".default = let
+      pkgs = import nixpkgs {
+        inherit system;
+      };
+    in pkgs.mkShell {
+      packages = with pkgs; [
+        # TODO: Use a smaller scheme with list of additional packages
+        texlive.combined.scheme-full
+      ];
+    };
+  };
+}
