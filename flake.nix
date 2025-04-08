88d76cc9b288f2c5e0824403564ecbadab3e2dd4
J. Ryan Stinnett <jryans@gmail.com>
2025-04-08 12:58:37 +0100

    Format flake.nix

--- flake.nix
+++ flake.nix
@@ -3,18 +3,23 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
   };
 
-  outputs = { self, nixpkgs, ... }: let
-    system = "x86_64-linux";
-  in {
-    devShells."${system}".default = let
-      pkgs = import nixpkgs {
-        inherit system;
-      };
-    in pkgs.mkShell {
-      packages = with pkgs; [
-        # TODO: Use a smaller scheme with list of additional packages
-        texlive.combined.scheme-full
-      ];
+  outputs =
+    { self, nixpkgs, ... }:
+    let
+      system = "x86_64-linux";
+    in
+    {
+      devShells."${system}".default =
+        let
+          pkgs = import nixpkgs {
+            inherit system;
+          };
+        in
+        pkgs.mkShell {
+          packages = with pkgs; [
+            # TODO: Use a smaller scheme with list of additional packages
+            texlive.combined.scheme-full
+          ];
+        };
     };
-  };
 }
