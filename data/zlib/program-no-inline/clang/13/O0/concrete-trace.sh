dfe654ba12b89c15a473c308db4fd625d1605cdf
J. Ryan Stinnett <jryans@gmail.com>
2025-04-16 15:29:30 +0100

    Add zlib concrete trace script

---
+++ data/zlib/program-no-inline/clang/13/O0/concrete-trace.sh
@@ -0,0 +1,49 @@
+#!/usr/bin/env bash
+set -ux
+
+SCRIPT_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" &> /dev/null && pwd)
+
+# Expects to run from this script's directory
+if [ "${PWD}" != "${SCRIPT_DIR}" ]; then
+  echo "Does not appear to be the expected directory, abort!"
+  exit
+fi
+
+TARGET_NAME="example"
+source "${SCRIPT_DIR}/../../../../../vars.sh"
+
+level="O0"
+version="13"
+echo "## Collecting concrete trace of \`${TARGET_NAME}\` (Clang ${version}, ${level})"
+
+# Each execution of the target to analyse
+executions=(example)
+example_COMMAND=""
+
+# Different trace variants to collect
+# These map to different trace options in `vars.sh`
+trace_variants=(default)
+
+for execution in ${executions[*]}; do
+  execution_command="${execution}_COMMAND"
+  echo "### Analysing execution of \`${TARGET_NAME} ${!execution_command}\`"
+
+  for trace_variant in ${trace_variants[*]}; do
+    trace_variant_opts="CON_TRACE_${trace_variant//-/_}_OPTS"
+    echo "#### Collecting trace variant \`${trace_variant}\`: ${!trace_variant_opts}"
+
+    mkdir -p concrete-trace/${execution}/${trace_variant}
+    (
+      cd concrete-trace/${execution}/${trace_variant};
+      env \
+        ${!trace_variant_opts} \
+        ${CON_COLLECT_INSTRUMENTATION} \
+        "$@" \
+        ../../../${TARGET_NAME} \
+        ${!execution_command} \
+        > stdout;
+      rm -rf foo.gz;
+      mv trace-* trace
+    )
+  done
+done
