12beeb89ea8cf15c6617e9ffd6b44d00d1e86fda
J. Ryan Stinnett <jryans@gmail.com>
2025-06-24 15:44:57 +0100

    Add FFmpeg program build script

---
+++ data/consistency-concrete/ffmpeg/program/prepare.sh
@@ -0,0 +1,44 @@
+#!/usr/bin/env bash
+set -eux
+
+if [ "${PWD##*/}" != "ffmpeg" ]; then
+  echo "Does not appear to be the expected directory, abort!"
+  exit
+fi
+
+SCRIPT_DIR=$(dirname "${BASH_SOURCE[0]}")
+source "${SCRIPT_DIR}/../../vars.sh"
+
+echo "## Preparing \`ffmpeg\`"
+
+# Configure
+# Focus on `ffmpeg` binary, disable other programs
+# Disable documentation
+# Disable platform-specific features
+# Disable debugging and optimisation defaults
+# Disable threads (Must use a commit before `760ce4bc0bd11f74f0851c0a662dd5cae888df83`)
+./configure \
+  --disable-ffplay \
+  --disable-ffprobe \
+  --disable-doc \
+  --disable-protocol=tls \
+  --disable-appkit \
+  --disable-avfoundation \
+  --disable-coreimage \
+  --disable-metal \
+  --disable-securetransport \
+  --disable-audiotoolbox \
+  --disable-videotoolbox \
+  --disable-asm \
+  --disable-debug \
+  --disable-optimizations \
+  --disable-stripping \
+  --disable-pthreads
+
+# Disable dependency tracking
+sd 'DEPFLAGS=.*$' 'DEPFLAGS=' ffbuild/config.mak
+
+# Remove compiler-specific options
+sd '\-mstack-alignment=16 \-Qunused-arguments ' '' ffbuild/config.mak
+sd '\-Werror=partial-availability ' '' ffbuild/config.mak
+sd ' -Wno-maybe-uninitialized' '' ffbuild/config.mak
