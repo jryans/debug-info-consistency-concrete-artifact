f933e9e95032715d17c1786a016181039f9cf0ac
J. Ryan Stinnett <jryans@gmail.com>
2025-07-28 14:16:41 +0100

    Add Git consistency scripts for Clang 18

---
+++ data/consistency-concrete/git/program/clang/18/O0/concrete-trace-tests.sh
@@ -0,0 +1,103 @@
+#!/usr/bin/env bash
+set -ux
+
+# Expects to run from program source tests subdirectory
+if [ "${PWD##*/}" != "t" ]; then
+  echo "Does not appear to be the expected directory, abort!"
+  exit
+fi
+
+SCRIPT_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" &> /dev/null && pwd)
+source "${SCRIPT_DIR}/../../../../../vars.sh"
+
+TARGET_NAME="git"
+
+level="O0"
+version="18"
+echo "## Collecting concrete trace of \`${TARGET_NAME}\` (Clang ${version}, ${level})"
+
+# Tests from the target's test suite to analyse
+tests=(
+  t0001-init
+  t1007-hash-object
+  # t1410-reflog: trace collection abysmally slow for some reason
+  # t2402-worktree-list
+  # t3201-branch-contains
+  # t3206-range-diff
+  # t3301-notes
+  # t4002-diff-basic
+  # t5505-remote
+)
+
+# Different trace variants to collect
+# These map to different trace options in `vars.sh`
+trace_variants=(default)
+
+# Remove dependencies from past runs
+rm -f "../${TARGET_NAME}"
+rm -f "../${TARGET_NAME}.dwarf"
+rm -rf "../bin-wrappers"
+rm -rf "../templates/blt"
+rm -f "../GIT-BUILD-OPTIONS"
+rm -f "../t/helper/test-tool"
+
+# Set up test dependencies
+ln -s "${SCRIPT_DIR}/${TARGET_NAME}" "../"
+ln -s "${SCRIPT_DIR}/${TARGET_NAME}.dwarf" "../"
+ln -s "${SCRIPT_DIR}/test-deps/bin-wrappers" "../"
+ln -s "${SCRIPT_DIR}/test-deps/blt" "../templates/"
+ln -s "${SCRIPT_DIR}/test-deps/GIT-BUILD-OPTIONS" "../"
+ln -s "${SCRIPT_DIR}/test-deps/test-tool" "../t/helper/"
+
+# TODO: Automatically modify `git` binary wrapper to add instrumentation
+# For now, this is done manually by adding:
+# ```
+# DYLD_INSERT_LIBRARIES=<path from `CON_COLLECT_INSTRUMENTATION`>
+# export DYLD_INSERT_LIBRARIES
+# ```
+
+for test in ${tests[*]}; do
+  echo "### Analysing execution of \`${TARGET_NAME}\` test \`${test}\`"
+
+  for trace_variant in ${trace_variants[*]}; do
+    trace_variant_opts="CON_TRACE_${trace_variant//-/_}_OPTS"
+    echo "#### Collecting trace variant \`${trace_variant}\`: ${!trace_variant_opts}"
+
+    # Setup `git` binary wrapper for this variant
+    ln -s \
+      "${SCRIPT_DIR}/test-deps/bin-wrappers/${trace_variant}/git" \
+      "${SCRIPT_DIR}/test-deps/bin-wrappers/git"
+
+    # Remove temporary trace collection from past runs
+    rm -rf traces
+    find . -name 'trace-*' -delete
+
+    mkdir -p ${SCRIPT_DIR}/concrete-trace/${test}/${trace_variant}
+    env \
+      "./${test}.sh" \
+      --debug
+
+    # Collect traces from all test processes
+    # Sorted by file creation time from oldest to newest
+    (
+      mkdir traces;
+      find . -name 'trace-*' -not -path './traces/*' -print0 | \
+        xargs -I % -0 mv % traces;
+      cd traces;
+      ls -tr | \
+        xargs cat \
+        > ${SCRIPT_DIR}/concrete-trace/${test}/${trace_variant}/trace
+    )
+
+    # Remove link to this variant's binary wrapper
+    rm -f "${SCRIPT_DIR}/test-deps/bin-wrappers/git"
+  done
+done
+
+# Cleanup
+rm -f "../${TARGET_NAME}"
+rm -f "../${TARGET_NAME}.dwarf"
+rm -rf "../bin-wrappers"
+rm -rf "../templates/blt"
+rm -f "../GIT-BUILD-OPTIONS"
+rm -f "../t/helper/test-tool"
---
+++ data/consistency-concrete/git/program/clang/18/O1/concrete-diff.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+set -ux
+
+SCRIPT_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" &> /dev/null && pwd)
+
+# Expects to run from this script's directory
+if [ "${PWD}" != "${SCRIPT_DIR}" ]; then
+  echo "Does not appear to be the expected directory, abort!"
+  exit
+fi
+
+TARGET_NAME="git"
+source "${SCRIPT_DIR}/../../../../../vars.sh"
+
+# Clang O1 checked against O0
+level="O1"
+version="18"
+echo "## Checking concrete debug consistency of \`${TARGET_NAME}\` (Clang ${version}, ${level})"
+
+mkdir -p diff
+git diff --no-index --diff-algorithm=myers -- \
+  ../O0/concrete-trace concrete-trace \
+  | diffstat -t -p 2 -S ../O0/concrete-trace \
+  > diff/myers
+git diff --no-index --diff-algorithm=minimal -- \
+  ../O0/concrete-trace concrete-trace \
+  | diffstat -t -p 2 -S ../O0/concrete-trace \
+  > diff/minimal
+git diff --no-index --diff-algorithm=patience -- \
+  ../O0/concrete-trace concrete-trace \
+  | diffstat -t -p 2 -S ../O0/concrete-trace \
+  > diff/patience
+git diff --no-index --diff-algorithm=histogram -- \
+  ../O0/concrete-trace concrete-trace \
+  | diffstat -t -p 2 -S ../O0/concrete-trace \
+  > diff/histogram
