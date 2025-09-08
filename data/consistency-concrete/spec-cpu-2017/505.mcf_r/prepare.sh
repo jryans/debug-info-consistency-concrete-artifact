a4208ba30e65aedfe99097625d8f7e733e0a4379
J. Ryan Stinnett <jryans@gmail.com>
2025-09-08 17:48:38 +0100

    Tweak SPEC CPU prepare notes

--- data/consistency-concrete/spec-cpu-2017/505.mcf_r/prepare.sh
+++ data/consistency-concrete/spec-cpu-2017/505.mcf_r/prepare.sh
@@ -7,5 +7,5 @@ set -eux
 # 2. Review quick start docs
 #    (https://www.spec.org/cpu2017/Docs/quick-start.html)
 # 3. Build the `505.mcf_r` benchmark:
-#    runcpu --config=clang-macos.cfg --action=build 505.mcf_r
+#    runcpu --config=gcc-linux-x86.cfg --action=buildsetup 505.mcf_r
 # 4. Run the `build.sh` here in the generated `build_base_mytest.0000` directory
