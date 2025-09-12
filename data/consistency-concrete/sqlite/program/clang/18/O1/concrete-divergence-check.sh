6c91a7b7a8b95bace92e37e7466a2c51892214b9
J. Ryan Stinnett <jryans@gmail.com>
2025-09-12 15:44:44 +0100

    Add SQLite divergence check script

---
+++ data/consistency-concrete/sqlite/program/clang/18/O1/concrete-divergence-check.sh
@@ -0,0 +1,87 @@
+#!/usr/bin/env bash
+set -eux
+
+SCRIPT_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" &> /dev/null && pwd)
+
+# Expects to run from this script's directory
+if [ "${PWD}" != "${SCRIPT_DIR}" ]; then
+  echo "Does not appear to be the expected directory, abort!"
+  exit
+fi
+
+TARGET_NAME="git"
+source "${SCRIPT_DIR}/../../../../../vars.sh"
+
+echo "## Checking concrete debug consistency of \`${TARGET_NAME}\`"
+
+# Using `LC_ALL=C` gives ~10x performance boost
+export LC_ALL=C
+
+# Collect divergence report, events, and counts for each test
+for test_path in concrete-trace/*; do
+  test=$(basename ${test_path})
+
+  # Collect divergence report and events by type
+  mkdir -p divergences/${test}/default
+  mkdir -p divergences/${test}/default/events
+  rm -f divergences/${test}/default/events/*
+  ${CON_COMPARE} \
+    --events-by-type-dir divergences/${test}/default/events \
+    ../O0/concrete-trace/${test}/default \
+    concrete-trace/${test}/default \
+    > divergences/${test}/default/divergences.md
+  # ${CON_COMPARE} \
+  #   --inlining-transform clustered \
+  #   --save-after-inlining-transform \
+  #   ../O0/concrete-trace/${test}/default \
+  #   concrete-trace/${test}/default \
+  #   -vv \
+  #   > divergences/${test}/default/divergences-clustered.md
+  # ${CON_COMPARE} \
+  #   --inlining-transform deduplicated \
+  #   --save-after-inlining-transform \
+  #   ../O0/concrete-trace/${test}/default \
+  #   concrete-trace/${test}/default \
+  #   -vv \
+  #   > divergences/${test}/default/divergences-deduplicated.md
+
+  # Count unique divergence lines by type
+  mkdir -p divergences/${test}/default/counts
+  rm -f divergences/${test}/default/counts/*
+  for divergence_type_path in divergences/${test}/default/events/*; do
+    divergence_type=$(basename ${divergence_type_path})
+    # These files do not include indentation
+    sort -u divergences/${test}/default/events/${divergence_type} | \
+      wc -l | \
+      awk '{print $1}' \
+      > divergences/${test}/default/counts/${divergence_type}
+  done
+
+  # Count unique lines in before trace overall
+  awk '{$1=$1};1' ../O0/concrete-trace/${test}/default/* | \
+    sort -u | \
+    wc -l | \
+    awk '{print $1}' \
+    > divergences/${test}/default/counts/before
+done
+
+# Aggregate unique line counts across all tests by type
+mkdir -p divergences/summary/default/counts
+rm -f divergences/summary/default/counts/*
+# Use the last test we happened to run as an example of available events
+for divergence_type_path in divergences/${test}/default/events/*; do
+  divergence_type=$(basename ${divergence_type_path})
+  cat divergences/*/default/events/${divergence_type} | \
+    sort -u | \
+    wc -l | \
+    awk '{print $1}' \
+    > divergences/summary/default/counts/${divergence_type}
+done
+
+# Aggregate unique lines in before trace across all tests
+cat ../O0/concrete-trace/*/default/* | \
+  awk '{$1=$1};1' | \
+  sort -u | \
+  wc -l | \
+  awk '{print $1}' \
+  > divergences/summary/default/counts/before
