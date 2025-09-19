c5b78fd743ad7701c69e68f2b4579bf6a29fe37d
J. Ryan Stinnett <jryans@gmail.com>
2025-09-19 17:37:51 +0100

    Add SPEC CPU 2017 500.perlbench_r build script

--- data/consistency-concrete/spec-cpu-2017/prepare.sh
+++ data/consistency-concrete/spec-cpu-2017/prepare.sh
@@ -6,6 +6,7 @@ set -eux
 # 1. Extract SPEC CPU 2017 distribution media
 # 2. Review quick start docs
 #    (https://www.spec.org/cpu2017/Docs/quick-start.html)
-# 3. Build the `505.mcf_r` benchmark:
-#    runcpu --config=gcc-linux-x86.cfg --action=buildsetup 505.mcf_r
-# 4. Run the `build.sh` here in the generated `build_base_mytest.0000` directory
+# 3. Build the `intrate` benchmarks:
+#    runcpu --config=gcc-linux-x86.cfg --action=buildsetup intrate
+# 4. Run the `build.sh` in each benchmark's generated
+#    `build_base_mytest-m64.0000` directory
