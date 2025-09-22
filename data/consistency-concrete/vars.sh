98b80a6710be48d5f230dd11ccd2f880ba637fac
J. Ryan Stinnett <jryans@gmail.com>
2025-09-22 14:30:48 +0100

    Add C++ stdlib paths to Linux sysroot

--- data/consistency-concrete/vars.sh
+++ data/consistency-concrete/vars.sh
@@ -40,7 +40,7 @@ klee() {
 if [[ "$OS" == 'mac' ]]; then
   CC_SYSROOT_OPTS="--sysroot /Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX.sdk"
 else
-  CC_SYSROOT_OPTS="-B/nix/store/maxa3xhmxggrc5v2vc0c3pjb79hjlkp9-glibc-2.40-66/lib/ -idirafter /nix/store/81awch8mhqanda1vy0c09bflgra4cxh0-glibc-2.40-66-dev/include -B/nix/store/yg4ahy7gahx91nq80achmzilrjyv0scj-gcc-13.3.0/lib/gcc/x86_64-unknown-linux-gnu/13.3.0 -B/nix/store/w9mcn0hi97xp5m9ds5nsc4cr99qspz9h-clang-18.1.8-lib/lib ${NIX_CFLAGS_COMPILE}"
+  CC_SYSROOT_OPTS="-isystem /nix/store/yg4ahy7gahx91nq80achmzilrjyv0scj-gcc-13.3.0/include/c++/13.3.0 -isystem /nix/store/yg4ahy7gahx91nq80achmzilrjyv0scj-gcc-13.3.0/include/c++/13.3.0/x86_64-unknown-linux-gnu -B/nix/store/maxa3xhmxggrc5v2vc0c3pjb79hjlkp9-glibc-2.40-66/lib/ -idirafter /nix/store/81awch8mhqanda1vy0c09bflgra4cxh0-glibc-2.40-66-dev/include -B/nix/store/yg4ahy7gahx91nq80achmzilrjyv0scj-gcc-13.3.0/lib/gcc/x86_64-unknown-linux-gnu/13.3.0 -B/nix/store/w9mcn0hi97xp5m9ds5nsc4cr99qspz9h-clang-18.1.8-lib/lib ${NIX_CFLAGS_COMPILE}"
 fi
 CC_COMMON_OPTS="${CC_SYSROOT_OPTS} -g -fno-pie"
 CC_CLANG_OPTS="-fno-discard-value-names -Xclang -disable-O0-optnone"
