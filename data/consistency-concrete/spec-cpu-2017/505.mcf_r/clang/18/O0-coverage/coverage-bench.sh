ca90f3b483d25f8454b792495a348140afb27f03
J. Ryan Stinnett <jryans@gmail.com>
2025-10-07 17:47:38 +0100

    Add SPEC CPU 2017 505.mcf_r coverage to evaluation details

---
+++ data/consistency-concrete/spec-cpu-2017/505.mcf_r/clang/18/O0-coverage/coverage-bench.sh
@@ -0,0 +1,43 @@
+#!/usr/bin/env bash
+set -ux
+
+SCRIPT_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" &> /dev/null && pwd)
+
+# Expects to run from program build directory
+if [ "${PWD##*/}" != "build_base_mytest-m64.0000" ]; then
+  echo "Does not appear to be the expected directory, abort!"
+  exit
+fi
+
+source "${SCRIPT_DIR}/../../../../../vars.sh"
+
+TARGET_NAME="mcf"
+INPUT="../../data/refrate/input/inp.in"
+
+echo "## Collecting coverage using benchmark inputs for SPEC CPU test"
+
+PROFILE_BASE_DIR="${SCRIPT_DIR}/coverage/bench"
+rm -rf ${PROFILE_BASE_DIR}
+PROFILE_DIR="${PROFILE_BASE_DIR}/profiles"
+mkdir -p ${PROFILE_DIR}
+LLVM_PROFILE_FILE="${PROFILE_DIR}/%p.profdata"
+export LLVM_PROFILE_FILE
+
+${SCRIPT_DIR}/${TARGET_NAME} \
+  ${INPUT}
+
+echo "## Merging raw profiles into indexed profile"
+
+find ${PROFILE_BASE_DIR} -type f -name '*.profdata' | \
+  $(llvm release-clang-lldb-18 llvm-profdata) \
+    merge \
+    -f - \
+    -o "${PROFILE_BASE_DIR}/merged.profdata"
+
+echo "## Reporting coverage"
+
+$(llvm release-clang-lldb-18 llvm-cov) \
+  report \
+  -instr-profile "${PROFILE_BASE_DIR}/merged.profdata" \
+  ${SCRIPT_DIR}/${TARGET_NAME} > \
+  "${PROFILE_BASE_DIR}/coverage"
---
+++ data/consistency-concrete/spec-cpu-2017/505.mcf_r/clang/18/O0-coverage/coverage-trace.sh
@@ -0,0 +1,43 @@
+#!/usr/bin/env bash
+set -ux
+
+SCRIPT_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" &> /dev/null && pwd)
+
+# Expects to run from this script's directory
+if [ "${PWD}" != "${SCRIPT_DIR}" ]; then
+  echo "Does not appear to be the expected directory, abort!"
+  exit
+fi
+
+source "${SCRIPT_DIR}/../../../../../vars.sh"
+
+TARGET_NAME="mcf"
+INPUT="${SCRIPT_DIR}/../../../data/input"
+
+echo "## Collecting coverage using trace inputs for SPEC CPU test"
+
+PROFILE_BASE_DIR="${SCRIPT_DIR}/coverage/trace"
+rm -rf ${PROFILE_BASE_DIR}
+PROFILE_DIR="${PROFILE_BASE_DIR}/profiles"
+mkdir -p ${PROFILE_DIR}
+LLVM_PROFILE_FILE="${PROFILE_DIR}/%p.profdata"
+export LLVM_PROFILE_FILE
+
+${SCRIPT_DIR}/${TARGET_NAME} \
+  ${INPUT}
+
+echo "## Merging raw profiles into indexed profile"
+
+find ${PROFILE_BASE_DIR} -type f -name '*.profdata' | \
+  $(llvm release-clang-lldb-18 llvm-profdata) \
+    merge \
+    -f - \
+    -o "${PROFILE_BASE_DIR}/merged.profdata"
+
+echo "## Reporting coverage"
+
+$(llvm release-clang-lldb-18 llvm-cov) \
+  report \
+  -instr-profile "${PROFILE_BASE_DIR}/merged.profdata" \
+  ${SCRIPT_DIR}/${TARGET_NAME} > \
+  "${PROFILE_BASE_DIR}/coverage"
