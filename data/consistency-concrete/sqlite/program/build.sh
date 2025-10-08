c448127a7c5080842807509b5f04735610e4731b
J. Ryan Stinnett <jryans@gmail.com>
2025-10-08 21:41:44 +0100

    Add SQLite coverage to evaluation details

--- data/consistency-concrete/sqlite/program/build.sh
+++ data/consistency-concrete/sqlite/program/build.sh
@@ -67,6 +67,28 @@ for i in ${!levels[*]}; do
     "${SCRIPT_DIR}/clang/${version}/${level}/${TARGET_NAME}"
 done
 
+# Clang source-based code coverage
+
+level="O0"
+version="18"
+echo "## Building \`${TARGET_NAME}\` (Clang ${version}, ${level}) for code coverage"
+
+make clean
+
+## Build for code coverage
+cc_level_opts="CC_${level}_OPTS"
+make \
+  CC="$(llvm release-clang-lldb-${version} clang)" \
+  CFLAGS="${CC_COMMON_OPTS} ${CC_CLANG_OPTS} ${!cc_level_opts} -fprofile-instr-generate -fcoverage-mapping -L/nix/store/jmxm0x5w0p5a1v849a0cpklsckjwj5nz-tcl-8.6.15/lib ${LD_COMMON_OPTS}" \
+  testfixture
+
+mkdir -p "${SCRIPT_DIR}/clang/${version}/${level}-coverage"
+
+## Store program binary
+cp \
+  ${TARGET_PATH} \
+  "${SCRIPT_DIR}/clang/${version}/${level}-coverage/${TARGET_NAME}"
+
 # GCC
 
 #   levels=(O0 O1 O2)
---
+++ data/consistency-concrete/sqlite/program/clang/18/O0-coverage/coverage-bench.sh
@@ -0,0 +1,48 @@
+#!/usr/bin/env bash
+set -ux
+
+SCRIPT_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" &> /dev/null && pwd)
+
+# Expects to run from program source directory
+if [ "${PWD##*/}" != "sqlite" ]; then
+  echo "Does not appear to be the expected directory, abort!"
+  exit
+fi
+
+source "${SCRIPT_DIR}/../../../../../vars.sh"
+
+TARGET_NAME="testfixture"
+
+echo "## Collecting coverage using default test suite"
+
+PROFILE_BASE_DIR="${SCRIPT_DIR}/coverage/bench"
+rm -rf ${PROFILE_BASE_DIR}
+PROFILE_DIR="${PROFILE_BASE_DIR}/profiles"
+mkdir -p ${PROFILE_DIR}
+LLVM_PROFILE_FILE="${PROFILE_DIR}/%p.profdata"
+export LLVM_PROFILE_FILE
+
+JOBS="$(echo "$(nproc) - 4" | bc)"
+
+${SCRIPT_DIR}/testfixture \
+  ./test/testrunner.tcl \
+  --jobs ${JOBS}
+
+echo "## Merging raw profiles into indexed profile"
+
+find ${PROFILE_BASE_DIR} -type f -name '*.profdata' | \
+  $(llvm release-clang-lldb-18 llvm-profdata) \
+    merge \
+    -f - \
+    -o "${PROFILE_BASE_DIR}/merged.profdata"
+
+# Remove raw profiles, takes up too much space
+rm -rf ${PROFILE_DIR}
+
+echo "## Reporting coverage"
+
+$(llvm release-clang-lldb-18 llvm-cov) \
+  report \
+  -instr-profile "${PROFILE_BASE_DIR}/merged.profdata" \
+  ${SCRIPT_DIR}/${TARGET_NAME} > \
+  "${PROFILE_BASE_DIR}/coverage"
