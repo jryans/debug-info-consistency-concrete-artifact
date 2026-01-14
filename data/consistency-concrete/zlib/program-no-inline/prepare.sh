d3f9e79fe26a2cdca6e042860f872f3b000fe1f6
J. Ryan Stinnett <jryans@gmail.com>
2026-01-14 16:26:24 +0000

    Remove scripts for unused projects

--- data/consistency-concrete/zlib/program-no-inline/prepare.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-set -eux
-
-if [ "${PWD##*/}" != "zlib" ]; then
-  echo "Does not appear to be the expected directory, abort!"
-  exit
-fi
-
-# Nothing to do, `configure` will be called for each build
