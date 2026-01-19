79aa3ead683e71ecdd1966064f4701ef39fa12c6
J. Ryan Stinnett <jryans@gmail.com>
2026-01-19 17:43:21 +0000

    Copy auto-jumping to FFmpeg and SQLite

--- data/consistency-concrete/sqlite/clang/18/O0/concrete-trace-tests.sh
+++ data/consistency-concrete/sqlite/clang/18/O0/concrete-trace-tests.sh
@@ -1,13 +1,23 @@
 #!/usr/bin/env bash
 set -ux
 
+SCRIPT_PATH=$(readlink -f "${BASH_SOURCE[0]}")
+SCRIPT_DIR=$(dirname "${SCRIPT_PATH}")
+
+# In bundled mode, jump to expected directory and invoke Nix shell
+if [ -n "${BUNDLED:-}" -a -z "${IN_NIX_SHELL:-}" ]; then
+  pushd /artifact/sqlite
+  nix develop --command bash "${SCRIPT_PATH}"
+  popd
+  exit
+fi
+
 # Expects to run from program source directory
 if [ "${PWD##*/}" != "sqlite" ]; then
   echo "Does not appear to be the expected directory, abort!"
   exit
 fi
 
-SCRIPT_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" &> /dev/null && pwd)
 source "${SCRIPT_DIR}/../../../../vars.sh"
 
 TARGET_NAME="testfixture"
